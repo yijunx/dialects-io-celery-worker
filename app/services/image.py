8030a5589545002539a04bea34a6161c17de1b9e
yijunxu <yijunx@umich.edu>
2021-11-16 16:26:18 +0800

    created folder structure

---
+++ app/services/image.py
@@ -0,0 +1,9 @@
+# resize user profile image.. and upload image to somewhere.. 
+# minio or s3 is needed to store user profile picture
+# when user upload pic, user management takes it, and 
+# save to minio, and let celery know, celery go and resize it, and delete the only one (of big size)
+# front end needs to restrict the photo size
+
+# user will have a link to the pic for the front end to render
+# browser go and fetch the pic without going through user-management
+
---
+++ app/services/user.py
@@ -0,0 +1,1 @@
+# to sync user info
